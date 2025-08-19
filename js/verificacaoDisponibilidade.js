cbf99110fb1bfb4700833e7525451ac0b6663479
Mike Araujo <contato.mikearaujo@gmail.com>
2025-08-19 16:24:06 -0300

    atualização 19/08 16:25

--- js/verificacaoDisponibilidade.js
+++ js/verificacaoDisponibilidade.js
@@ -1,88 +1,128 @@
-
-// Tipo de veiculo
+// INPUT: Tipo de veiculo
 let containerOpcoesTipoVeiculos = document.querySelector('.opcoes-tipo-veiculo');
 let opcoesTipoVeiculos = document.querySelectorAll('.opcoes-tipo-veiculo>ul>li');
 let inputVeriTipoVeiculo = document.querySelector('#input-veri-1');
-let abrirTipoVeiculo = document.querySelector('#abrir-tipo-veiculos');
-let countTipoveiculo = 0
+let popupCarros = false
 
+document.querySelector('#abrir-tipo-veiculos').addEventListener('click', exibirPopupCarros)
+
+function exibirPopupCarros() {
+    if (popupCarros == false) {
+        containerOpcoesTipoVeiculos.style.animationName = 'aparecer'
+        popupCarros = true
+    } else {
+        containerOpcoesTipoVeiculos.style.animationName = 'desaparecer'
+        popupCarros = false
+    }
+}
 
 opcoesTipoVeiculos.forEach(i => {
-    i.addEventListener('click', () => {
-        let nome = i.textContent
-        inputVeriTipoVeiculo.placeholder = nome
-        containerOpcoesTipoVeiculos.style.animationName = 'desaparecer'
-        countTipoveiculo = 0
+    selecionarItemCarros(i)
+})
 
+function selecionarItemCarros(item) {
+    item.addEventListener('click', () => {
+        let nome = item.textContent
+        atualizarInputComValorCarros(nome)
     })
-})
+}
+
+function atualizarInputComValorCarros(nome) {
+    inputVeriTipoVeiculo.value = nome
+    ocultarPopupCarros()
+}
+
+let ocultarPopupCarros = () => {
+    containerOpcoesTipoVeiculos.style.animationName = 'desaparecer'
+    popupCarros = false
+}
 
 
-abrirTipoVeiculo.addEventListener('click', (e) => {
-
-    if (countTipoveiculo == 0) {
-        containerOpcoesTipoVeiculos.style.animationName = 'aparecer'
-        countTipoveiculo = 1
-    } else if (countTipoveiculo == 1) {
-        containerOpcoesTipoVeiculos.style.animationName = 'desaparecer'
-        countTipoveiculo = 0
-    }
-})
 
 
-// Ano de veiculo
+// INPUT: Ano do veiculo
 
 let containerOpcoesAnoVeiculos = document.querySelector('.opcoes-ano-veiculo');
 let inputVeriAnoVeiculo = document.querySelector('#input-veri-2');
-let abrirAnoVeiculo = document.querySelector('#abrir-ano-veiculos');
 let listaAnosVeiculos = document.querySelector('#lista-de-anos-veiculos');
-let listaAno = []
-let countAnoVeiculo = 0
 
-abrirAnoVeiculo.addEventListener('click', (e) => {
+let popupAnoCarros = false
 
-    if (countAnoVeiculo == 0) {
+document.querySelector('#abrir-ano-veiculos').addEventListener('click', exibirPopupAnoCarros)
+
+function exibirPopupAnoCarros() {
+    if (popupAnoCarros == false) {
         containerOpcoesAnoVeiculos.style.animationName = 'aparecer'
-        countAnoVeiculo = 1
-    } else if (countAnoVeiculo == 1) {
+        popupAnoCarros = true
+        renderizarLista()
+    } else {
         containerOpcoesAnoVeiculos.style.animationName = 'desaparecer'
-        countAnoVeiculo = 0
+        popupAnoCarros = false
     }
-})
-
-
-for (let i = 2000; i <= 2025; i++) {
-    listaAno.push(i)
 }
 
-listaAno.forEach((i) => {
-    listaAnosVeiculos.innerHTML += `<li>${i}</li>`
-})
+function renderizarLista() {
+    let listaAno = []
 
-let opcoesAnoVeiculos = document.querySelectorAll('.opcoes-ano-veiculo>ul>li');
-
-opcoesAnoVeiculos.forEach(i => {
-    i.addEventListener('click', () => {
-        let ano = i.textContent
-        inputVeriAnoVeiculo.placeholder = ano
-        containerOpcoesAnoVeiculos.style.animationName = 'desaparecer'
-        countAnoVeiculo = 0
+    for (let i = 2000; i <= 2025; i++) {
+        listaAno.push(i)
+    }
+    listaAno.forEach((i) => {
+        listaAnosVeiculos.innerHTML += `<li onclick="selecionarAnoCarros(this)">${i}</li>`
     })
-})
+}
 
+function selecionarAnoCarros(ano) {
+    inputVeriAnoVeiculo.value = ano.innerHTML
+    ocultarPopupAnoCarros()
+}
 
-// cep
+let ocultarPopupAnoCarros = () => {
+    containerOpcoesAnoVeiculos.style.animationName = 'desaparecer'
+    popupAnoCarros = false
+}
 
+// INPUT: CEP
 
+let meuCep = document.querySelector('#meuCep')
 document.querySelectorAll('[data-mask="cep"]').forEach((el) => {
-  el.addEventListener('input', () => el.value = formatCEP(el.value));
-  el.addEventListener('blur', () => {
-    const ok = /^\d{5}-\d{3}$/.test(el.value);
-    el.setCustomValidity(ok || el.value === '' ? '' : 'CEP inválido');
-  });
-});
+    el.addEventListener('input', () => el.value = formatCEP(el.value));
+    el.addEventListener('blur', () => {
+        const ok = /^\d{5}-\d{3}$/.test(el.value);
+        el.setCustomValidity(ok || el.value === '' ? '' : 'CEP inválido');
+    });
+})
 
 function formatCEP(v) {
-  const d = v.replace(/\D/g, '').slice(0, 8);
-  return d.length <= 5 ? d : d.slice(0, 5) + '-' + d.slice(5);
+    const d = v.replace(/\D/g, '').slice(0, 8);
+    return d.length <= 5 ? d : d.slice(0, 5) + '-' + d.slice(5);
 }
+
+
+
+// Envio form
+document.querySelector('#verificarDisponibilidade').addEventListener('click', () => {
+    let tipo = inputVeriTipoVeiculo.value
+    let ano = inputVeriAnoVeiculo.value
+    let cep = meuCep.value
+
+    if (tipo == "") {
+        console.log('campo tipo vazio')
+    } else if (ano == "") {
+        console.log('campo ano vazio')
+    } else if (cep == "" || cep.length < 9) {
+        console.log('campo cep vazio ou incorreto')
+    } else {
+        console.log(`Tudo certo. Seu veiculo é: ${tipo}, o ano é ${ano}, e seu cep é ${cep}`)
+
+        fetch(`https://viacep.com.br/ws/${cep}/json/`)
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error(`Erro na requisição: ${res.status}`);
+                }
+                return res.json();
+            })
+            .then(data => console.log(data))
+            .catch(err => console.error("Falha ao consultar CEP:", err));
+    }
+})
