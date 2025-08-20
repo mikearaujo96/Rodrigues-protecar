967ef3b3842973a12f7840c777701746fa8270c4
Mike Araujo <contato.mikearaujo@gmail.com>
2025-08-20 19:07:52 -0300

    atualização 20/07 19:07

---
+++ js/sectionProdutos.js
@@ -0,0 +1,27 @@
+// const section = document.querySelector('.section-produto');
+// let containerProdutos1 = document.querySelector('.titulo-produtos')
+// let containerProdutos2 = document.querySelector('.conteudo-section-produto')
+// let containerProdutos3 = document.querySelector('.carregamento-section-produtos')
+// let containerProdutos4 = document.querySelector('.galeria-produto')
+// let containerProdutos5 = document.querySelector('.carregamento-section-produtos')
+
+
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//             containerProdutos1.style.animationName = 'aparecer'
+//             containerProdutos2.style.animationName = 'aparecer'
+//             containerProdutos3.style.animationName = 'aparecer'
+//             containerProdutos4.style.animationName = 'aparecer'
+//             containerProdutos5.style.animationName = 'aparecer'
+//         } else {
+//             containerProdutos1.style.animationName = ''
+//             containerProdutos2.style.animationName = ''
+//             containerProdutos3.style.animationName = ''
+//             containerProdutos4.style.animationName = ''
+//             containerProdutos5.style.animationName = ''
+//         }
+//     });
+// }, { threshold: 0.5 }); // 0.5 = 50% da altura da seção precisa estar visível
+
+// observer.observe(section);
