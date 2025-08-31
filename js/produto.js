43296ae0378e3e9a855fe027c3b02adb576fab09
Mike Araujo <contato.mikearaujo@gmail.com>
2025-08-30 21:35:21 -0300

    atualização 30/08 21:35

---
+++ js/produto.js
@@ -0,0 +1,46 @@
+const barra = document.querySelector('.carregamento-section-produtos span');
+const imgPrincipal = document.getElementById('minhaImagem-produto');
+const galeria = document.querySelectorAll('.galeria-produto img');
+
+const imagens = [
+    'assets/imagens/produto-angulo1.png',
+    'assets/imagens/produto-angulo2.png',
+    'assets/imagens/produto-angulo3.png'
+];
+
+let indice = 0;
+
+// Atualiza a galeria com a sequência correta
+function atualizarGaleria(indicePrincipal) {
+    const novaGaleria = [];
+    for (let i = 1; i <= imagens.length; i++) {
+        novaGaleria.push(imagens[(indicePrincipal + i) % imagens.length]);
+    }
+    galeria.forEach((img, i) => {
+        img.src = novaGaleria[i];
+    });
+}
+
+barra.addEventListener('animationiteration', () => {
+    // Aplica a classe de saída
+    imgPrincipal.classList.add('sair');
+
+    // Espera a transição de saída terminar
+    setTimeout(() => {
+        // Atualiza o índice da imagem principal
+        indice = (indice + 1) % imagens.length;
+        imgPrincipal.src = imagens[indice];
+
+        // Remove sair e adiciona entrar
+        imgPrincipal.classList.remove('sair');
+        imgPrincipal.classList.add('entrar');
+
+        // Atualiza a galeria
+        atualizarGaleria(indice);
+
+        // Remove a classe entrar após a animação
+        setTimeout(() => {
+            imgPrincipal.classList.remove('entrar');
+        }, 700);
+    }, 700); // tempo da transição de saída
+});
