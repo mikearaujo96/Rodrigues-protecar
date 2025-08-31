aaf419e9e972a908fe7ca573751357b938931871
Mike Araujo <contato.mikearaujo@gmail.com>
2025-08-31 19:45:29 -0300

    atualização 31/08 19:45

---
+++ js/videosFundo.js
@@ -0,0 +1,20 @@
+const video = document.getElementById("meuVideo2");
+  const container = document.getElementById("bgVideo2");
+
+  // força atributos importantes
+  video.muted = true;
+  video.setAttribute("muted", "");
+  video.setAttribute("playsinline", "");
+  video.setAttribute("webkit-playsinline", "");
+
+  // tenta dar play
+  video.play().catch(() => {
+    console.warn("Autoplay bloqueado, trocando por imagem estática.");
+    // remove o vídeo
+    video.remove();
+    // cria a imagem
+    const img = document.createElement("img");
+    img.src = "assets/imagens/backgroun-precos.png"; // coloque a imagem estática aqui
+    img.className = "video"; // pra herdar o mesmo CSS
+    container.appendChild(img);
+  });
